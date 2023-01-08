2becfe6e871582cee8d897ee4cbd3893e3c73e79
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-01-08 09:19:29 +0100

    added comment

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	car1 := shops.NewCar("Mercedes", "GLK", "red", 50000, 2, 2020)
 	car2 := shops.NewCar("Toyota", "Camry", "blue", 3000, 7, 3018)
-	//car3 := shops.NewCar("Lexus", "VLC", "blue", 0, 3000, 3018)
+	car3 := shops.NewCar("Lexus", "VLC", "blue", 3000, 6, 3018)
 
 	// instantiate a new store
 	store := shops.NewStore()
@@ -17,24 +17,23 @@ func main() {
 	//  adding item
 	store.AddItem(car1)
 	store.AddItem(car2)
-	//store.AddItem(car3)
+	store.AddItem(car3)
 
-	// listing all products in the store
+	// listing all available products in the store
 	store.AvailableProduct()
-	//fmt.Println()
+	fmt.Println()
 
 	// sell item
 	store.SellItem(car1, 1)
 	store.SellItem(car2, 4)
-	//store.SellItem(car2, 5)
-
-	// testing
-	store.AvailableProduct()
-	fmt.Println()
+	store.SellItem(car3, 5)
 
 	// list all sold item
 	store.ListSoldItem()
 	fmt.Println()
-	// store.ListAllProduct()
+
+	// listing all available products in the store
+	store.AvailableProduct()
+	fmt.Println()
 
 }
