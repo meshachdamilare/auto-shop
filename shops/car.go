370f765730bc489fe5f8e2bba5141377a65c8ddc
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-01-08 09:19:43 +0100

    added comment

--- shops/car.go
+++ shops/car.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// A Car is a type of product because it satisfied the Product interface
 type Car struct {
 	ID                string  `json:"id"`
 	Brand             string  `json:"brand"`
@@ -14,7 +15,7 @@ type Car struct {
 	YearOfManufacture int     `json:"yearOfManufacture"`
 }
 
-// NewCar ... creates a new car type during use
+// Initialize a new Car
 func NewCar(brand, model, color string, price float64, quantity, year int) *Car {
 	return &Car{
 		ID:                GenerateID(),
@@ -27,42 +28,52 @@ func NewCar(brand, model, color string, price float64, quantity, year int) *Car 
 	}
 }
 
+// Returns car name
 func (c *Car) GetName() string {
 	return c.Brand + " " + c.Model
 }
 
+// Returns car Id
 func (c *Car) GetID() string {
 	return c.ID
 }
 
+// Returns car quantity
 func (c *Car) GetQuantity() int {
 	return c.Quantity
 }
 
+// Modifies car quantity
 func (c *Car) SetQuantity(q int) {
 	c.Quantity = q
 }
 
+// Returns car price
 func (c *Car) GetPrice() float64 {
 	return c.Price
 }
 
+// Returns car brand
 func (c *Car) GetBrand() string {
 	return c.Brand
 }
 
+// Returns car model
 func (c *Car) GetModel() string {
 	return c.Model
 }
 
+// Returns car color
 func (c *Car) GetColor() string {
 	return c.Color
 }
 
+// Returns car year of manufacture
 func (c *Car) GetYearOfManufacture() int {
 	return c.YearOfManufacture
 }
 
+// Return a compact struct of car
 func (c *Car) DisplayProduct() any {
 	v := &Car{
 		ID:                c.GetID(),
@@ -76,6 +87,7 @@ func (c *Car) DisplayProduct() any {
 	return v
 }
 
+// Displays the status of car: in stock or out of stock
 func (c *Car) DisplayProductStatus() {
 	if c.GetQuantity() > 0 {
 		fmt.Println("Product In stock")
