981752a53079ba39562c4ffb437cdcc81719b594
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-01-09 23:05:12 +0100

    implement getter and setter

--- shops/product.go
+++ shops/product.go
@@ -2,15 +2,25 @@ package shops
 
 // Product interface: any product that has all these methods implement this interface
 type ProductItem interface {
+	ProductGetter
+	ProductSetter
+	DisplayProduct() any
+	DisplayProductStatus()
+}
+
+// Get the value of the Product Field
+type ProductGetter interface {
 	GetName() string
 	GetID() string
 	GetQuantity() int
 	GetPrice() float64
-	SetQuantity(int)
 	GetBrand() string
 	GetModel() string
 	GetColor() string
 	GetYearOfManufacture() int
-	DisplayProduct() any
-	DisplayProductStatus()
+}
+
+// To set value for the Product fields
+type ProductSetter interface {
+	SetQuantity(int)
 }
